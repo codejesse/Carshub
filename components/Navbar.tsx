75b306573cac709ff5a062664864d9340e34fb0a
Jesse <jessebeke10@gmail.com>
2023-07-12 15:14:37 +0100

    Renamed project to zoola car marketplace

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,13 +7,13 @@ import CustomButton from './CustomButton'
 const Navbar = () => {
   return (
     <header className='w-full absolute z-10 fixed'>
-        <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4'>
+        <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 ml-[-30px]'>
             <Link href="/" className='flex justify-center items-center'>
                 <Image
-                 src="/logo.svg"
+                 src="/zoolo.png"
                  alt='Car Hub Logo'
-                 width={118}
-                 height={18}
+                 width={218}
+                 height={28}
                  className='object-contain'
                 />
             </Link>
