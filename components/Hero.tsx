b66a64ddfc5120595e50fa2c3b8edd63104cb425
Jesse <jessebeke10@gmail.com>
2023-07-12 10:01:50 +0100

    Hero styling commit

---
+++ components/Hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Hero = () => {
+  return (
+    <div className="hero">
+        <div className='flex-1 pt-36 padding-x'>
+            <h1 className='hero__title'>
+                Find, book, or rent a supercar — Quickly and easily!
+            </h1>
+            {/* Study the styling and make changes of your own after build */}
+        </div>
+    </div>
+  )
+}
+
+export default Hero
