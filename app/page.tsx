9db520a2ba6007821077574802aca2761bedf3bb
Jesse <jessebeke10@gmail.com>
2023-07-12 12:06:40 +0100

    Time to create the car inventory section

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,18 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      {/* Catalogue div for car inventory */}
+      <div id="discover" className='mt-12 padding-x padding-y max-width'>
+        <div className="home__text-container">
+          <h1 className='text-4xl font-extrabold'>Car inventory</h1>
+          <p>Explore our inventory</p>
+
+          {/* filters div */}
+          <div className='home__filter'>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
