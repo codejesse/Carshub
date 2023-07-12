26bd8d05b1478218cb072d0956cda9db64b733a9
Jesse <jessebeke10@gmail.com>
2023-07-12 17:04:20 +0100

    Combobox autocomplete component finishing

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -17,7 +17,7 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
 
     return (
         <div className="search-manufacturer">
-            <Combobox>
+            <Combobox value={manufacturer} onChange={setManufacturer}>
                 <div className="relative w-full">
                     <Combobox.Button className="absolute top-[14px]">
                         <Image
@@ -56,7 +56,19 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
                                         value={item}
                                         className={({ active }) => `relative search-manufacturer__option ${active ? `bg-primary-blue text-white` : `text-gray-900`}`}
                                     >
-                                        {item}
+                                        {({ selected, active }) => (
+                                            <>
+                                                <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
+                                                    {item}
+                                                </span>
+
+                                                {/* Show an active blue background color if the option is selected */}
+                                                {selected ? (
+                                                    <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active ? "text-white" : "text-pribg-primary-purple"}`}
+                                                    ></span>
+                                                ) : null}
+                                            </>
+                                        )}
                                     </Combobox.Option>
                                 ))
                             )}
