5f82fad47e4b75c3c52b86e5840bd9e2056abd26
Jesse <jessebeke10@gmail.com>
2023-07-13 09:32:19 +0100

    Creating the car datadisplay modal

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import { CarProps } from '@/types'
 import CustomButton from './CustomButton'
 import { calculateCarRent } from '@/utils'
+import CarDetails from './CarDetails'
 
 interface CarCardProps {
     car: CarProps;
@@ -67,8 +68,9 @@ const CarCard = ({ car }: CarCardProps) => {
                         handleClick={() => setIsOpen(true)}
                     />
                 </div>
-
             </div>
+            
+            <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car} />
         </div>
     )
 }
